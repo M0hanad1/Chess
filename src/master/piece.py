d7b71cb7136c94a2fe1bb1a83171b3c350e9b572
M0hanad1 <mohned.dabsha@gmail.com>
2022-11-24 23:53:58 +0200

    Added knight moves.

--- src/master/piece.py
+++ src/master/piece.py
@@ -1,27 +1,41 @@
 class Piece:
+    """Chess pieces master class.
+
+        Parameters
+        ----------
+        position : list[int]
+            Piece position on the board [x, y].
+        positions_to_move : list[list]
+            Positions that the piece can move to [[x, y], [x, y]].
+        color : str
+            Piece color [white, black].
+    """
     POSITIONS_X = ["a", "b", "c", "d", "e", "f", "g", "h"]
     POSITIONS_Y = [1, 2, 3, 4, 5, 6, 7, 8]
     pieces = []
-    """Chess pieces master class.
-
-        Args:
-            position (list[int]): Piece position on the board [x, y].
-            positions_to_move (list[list]): Positions that the piece can move to.
-            color (str): Piece color [white, black]."""
 
     def __init__(self, position: list[int], positions_to_move: list[list], color: str) -> None:
         self.position = position
-        self.positions_to_move = positions_to_move
+        self.available_positions = positions_to_move
         self.color = color
         Piece.pieces.append(self)
 
-    def check_position_available(self, positions: list[list]) -> bool:
+    def check_available_positions(self, positions: list[list]) -> None:
+        """Check if the positions are available.
+
+        Parameters
+        ----------
+        positions : list[list]
+            All available positions of the piece [[x, y], [x, y]].
+        """
+        positions_result = positions
+
         for i in self.pieces:
-            if i.position in positions:
-                return False
+            if i.position in positions and i.color == self.color:
+                positions_result.remove(i.position)
 
         for i in positions:
-            if i[0] >= len(self.POSITIONS_X) or i[1] >= len(self.POSITIONS_Y):
-                return False
+            if (i[0] >= 8 or i[0] < 0) or (i[1] >= 8 or i[1] < 0):
+                positions_result.remove(i)
 
-        return True
+        self.available_positions = positions_result
--- src/pieces/knight.py
+++ src/pieces/knight.py
@@ -5,3 +5,10 @@ class Knight(Piece):
     def __init__(self, position: list[int], positions_to_move: list[list], color: str):
         super().__init__(position, positions_to_move, color)
 
+    def get_available_position(self):
+        self.check_available_positions([
+            [self.position[0]+2, self.position[1]+1], [self.position[0]-2, self.position[1]+1],
+            [self.position[0]+2, self.position[1]-1], [self.position[0]-2, self.position[1]-1],
+            [self.position[0]+1, self.position[1]+2], [self.position[0]+1, self.position[1]-2],
+            [self.position[0]-1, self.position[1]+2], [self.position[0]-1, self.position[1]-2],
+        ])
